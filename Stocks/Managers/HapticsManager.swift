1f8a0dc4cc0ed273fbca2e3b98c02bdd9e7fbbac
nsemirdev <nsemirdev@gmail.com>
2023-01-17 04:43:45 +0300

    implemented skeleton for hapticsmanager

--- Stocks/Managers/HapticsManager.swift
+++ Stocks/Managers/HapticsManager.swift
@@ -5,9 +5,16 @@
 //  Created by Emir Alkal on 17.01.2023.
 //
 
-import Foundation
+import UIKit
 
 final class HapticsManager {
     static let shared = HapticsManager()
     private init() {}
+    
+    // MARK: - Public
+    
+    public func vibrateForSelection() {
+        
+    }
+    
 }
