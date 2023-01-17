6d996a96232968966b065a094cb438f73cb1a11f
nsemirdev <nsemirdev@gmail.com>
2023-01-17 04:23:15 +0300

    embed in navbar root view controller

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -11,6 +11,7 @@ final class WatchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
     }
     
 }
