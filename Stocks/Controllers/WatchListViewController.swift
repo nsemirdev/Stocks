f3a2df1e55ec8f1816562c1ed066016cc8850ff8
nsemirdev <nsemirdev@gmail.com>
2023-01-17 05:08:34 +0300

    added custom title view and UIView extension

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -12,6 +12,37 @@ final class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
+        setUpTitleView()
+    }
+    
+    private func setUpTitleView() {
+        let titleView = UIView(frame: .init(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        
+        let label = UILabel(frame: .init(x: 10, y: 0, width: titleView.width - 20, height: titleView.height))
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 36, weight: .medium )
+        titleView.addSubview(label)
+        navigationItem.titleView = titleView
+    }
+    
+    private func setUpSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+    
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let _ = searchController.searchResultsController as? SearchResultsViewController,
+                  !query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        
+        print(query)
+        
     }
     
 }
---
+++ Stocks/Extensions/UIView+.swift
@@ -0,0 +1,34 @@
+//
+//  UIView+.swift
+//  Stocks
+//
+//  Created by Emir Alkal on 17.01.2023.
+//
+
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
+}
