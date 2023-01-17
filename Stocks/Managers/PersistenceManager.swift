5f32d488c56eb389692c44b932e760914fe3a7be
nsemirdev <nsemirdev@gmail.com>
2023-01-17 04:42:16 +0300

    implemented skeleton persistencemanager

--- Stocks/Managers/PersistenceManager.swift
+++ Stocks/Managers/PersistenceManager.swift
@@ -10,4 +10,30 @@ import Foundation
 final class PersistenceManager {
     static let shared = PersistenceManager()
     private init() {}
+    
+    private let userDefaults: UserDefaults = .standard
+    
+    private struct Constants {
+        
+    }
+    
+    // MARK: - Public
+    
+    public var watchlist: [String] {
+        []
+    }
+    
+    public func addToWatchlist() {
+        
+    }
+    
+    public func remoteFromWatchlist() {
+        
+    }
+    
+    // MARK: - Private
+    
+    private var hasOnboarded: Bool {
+        false
+    }
 }
